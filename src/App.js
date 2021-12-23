6973f9c7b3aadeb6e3de88b37f8199e7e11f3031
Amandeep0216 <amansaxena051@gmail.com>
2021-12-23 14:09:33 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import About from './About';
+import Contact from './Contact';
+import Home from './Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Link , BrowserRouter , Routes , Route} from 'react-router-dom'
+
+class App extends React.Component
+{
+    constructor()
+    {
+      super()   
+    }
+    render()
+    {     
+      return <div>
+         <h1>App Component</h1>
+        
+        <BrowserRouter>
+
+            <Link to="/"> <b>Home</b>  </Link> &nbsp;
+            <Link to="/about"> <b>About</b>  </Link>  &nbsp;
+            <Link to="/contact"> <b>Contact</b> </Link>  &nbsp;
+            <hr/>
+
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/contact" element={<Contact/>}/>
+            </Routes>
+            
+         </BrowserRouter>
+      </div>
+    }
 }
 
 export default App;
+
+
+
+
+
+/* import React from 'react'
+import Demo from './Mycomponent';
+
+class App extends React.Component
+{
+    constructor()
+    {
+      super()      
+      this.state = {
+          name : "Aman Deep"
+      }
+    }
+
+    changeName = (name)=>{
+      this.setState({name:name})
+    }
+
+    render()
+    {     
+      return <div>
+          <h1>App Component</h1>   
+          <b>{this.state.name}</b>
+
+          <br/>
+          <input type='text' placeholder='Name' 
+          onChange={(event)=>this.setState({name:event.target.value})}
+          value={this.state.name}/>
+
+          <br/>
+          <Demo data={this.state.name} fun={this.changeName}/>
+      </div>
+    }
+}
+
+export default App; */
